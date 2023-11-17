8eb9242bf743ea48c5da7e584ad5a18c449b305f
VIDI X <68710971+VIDI-X@users.noreply.github.com>
2023-11-17 10:45:45 +0100

    Update BLE_client.py

--- Raspberry_Pi/BLE_client.py
+++ Raspberry_Pi/BLE_client.py
@@ -1,18 +1,13 @@
-from bluetooth import *
 from bluepy import btle
 import RPi.GPIO as GPIO
-import time
-import struct
 
 ESP32_MAC_ADDR = "A4:E5:7C:FB:F8:D6"
 
 SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 MOTOR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-BALL_UUID = "88ae53aa-72ad-44e4-ac95-8e5f7a4a0bd2"
+MECHANISM_UUID = "88ae53aa-72ad-44e4-ac95-8e5f7a4a0bd2"
 
 setup_data = b'\x01\x00'
-
-stopped = True
 
 # enkoder motor
 mota = 24
@@ -20,28 +15,24 @@ motb = 25
 signal = 27
 el_pow = 20
 
-# servomotori za mehanizam
+# servomotori za pokretanje mehanizma
 servoA = 12
-#servo_L2 = 13
 servoB = 22
-#servo_R2 = 23
 
+# setup
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) # definira raspored GPIO pinova
 
 GPIO.setup(mota, GPIO.OUT)
 GPIO.setup(motb, GPIO.OUT)
 GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(el_pow, GPIO.OUT)
-GPIO.output(el_pow, GPIO.HIGH) #3.3V
-
+GPIO.output(el_pow, GPIO.HIGH)
 left = GPIO.PWM(mota, 100)
 right = GPIO.PWM(motb, 100)
 
 GPIO.setup(servoA, GPIO.OUT)
-#GPIO.setup(servo_L2, GPIO.OUT)
 GPIO.setup(servoB, GPIO.OUT)
-#GPIO.setup(servo_R2, GPIO.OUT)
 
 servo1 = GPIO.PWM(servoA, 100)
 servo2 = GPIO.PWM(servoB, 100)
@@ -50,78 +41,44 @@ servo1.start(0)
 servo2.start(0)
 
 active = right
-active.start(0)
-
-
-def Forward(motor, val):
-    motor.ChangeDutyCycle(val)
+stop = True
 
 def MotorControl(drive):
-    global active, stopped
-    global left, right
-    global mota, motb
-    if drive == b'\x01\x00':
-        print("Going left")
-        
+    global active, left, right, stop
+    # čitanje podatka (binarni zapis) sa VIDI X
+    if drive == b'\x01\x00':    # LIJEVO
         if (active == right):
             active = left
-            
-            right.stop()
-            GPIO.output(motb, GPIO.LOW)
-            #GPIO.cleanup()
-            
-            #lijevi = GPIO.PWM(motb1, 50)
-            #desni = GPIO.PWM(motb2, 100)
-            left.start(0)
-        
-        if (stopped == True):
-            stopped = False
-            Forward(left, 100)
-        
-    elif drive == b'\x02\x00':
-        print("Going right")
-        
+            if (stop == False):
+                right.stop()
+        left.start(100)
+        stop = False
+    elif drive == b'\x02\x00':  # DESNO
         if (active == left):
             active = right
-            
-            left.stop()
-            GPIO.output(mota, GPIO.LOW)
-            #GPIO.cleanup()
-            
-            #lijevi = GPIO.PWM(mota1, 50)
-            #desni = GPIO.PWM(mota2, 100)
-            
-            right.start(0)
-        
-        if (stopped == True):
-            stopped = False
-            Forward(right, 100)
-        
-    elif drive == b'\x00\x00':
-        print("Stopping")
-        if (stopped == False):
-            stopped = True
-            Forward(active, 0) #Forward(lijevi, desni, 0)
+            if (stop == False):
+                left.stop()
+        right.start(100)
+        stop = False
+    elif drive == b'\x00\x00':  # STOP
+        active.stop()
+        stop = True
 
-def BallControl(value):
-    if value == b'\x00\x00':
-        Forward(servo1, 100)
-        Forward(servo2, 100)
-    elif value == b'\x01\x00':
-        Forward(servo1, 0)
-        Forward(servo2, 0)
-
+def MechanismControl(value):
+    # čitanje podatka (binarni zapis) sa VIDI X
+    if value == b'\x00\x00':    # ON
+        servo1.start(100)
+        servo2.start(100)
+    elif value == b'\x01\x00':  # OFF
+        servo1.stop()
+        servo2.stop()
 
 def receive_notify(handle, data):
-    global ESP32
-    
-    #val = ESP32.readCharacteristic(handle)
-    print(handle, data)
-    if handle == 42:
+    global motor_handle, mechanism_handle
+    if handle == motor_handle:
         MotorControl(data)
-    elif handle == 45:
-        BallControl(data)
-
+    elif handle == mechanism_handle:
+        MechanismControl(data)
 
 class PeripheralDelegate(btle.DefaultDelegate):
     def __init__(self, callback):
@@ -129,54 +86,36 @@ class PeripheralDelegate(btle.DefaultDelegate):
         self.callback = callback
 
     def handleNotification(self, handle, data):
-        print("Handling notification...")
         self.callback(handle, data)
-
 
 try:
     ESP32 = btle.Peripheral(ESP32_MAC_ADDR)
     
-    motor_delegate = PeripheralDelegate(receive_notify)
-    ESP32.setDelegate(motor_delegate)
-    
+    delegate = PeripheralDelegate(receive_notify)
+    ESP32.setDelegate(delegate)
+
+    # Saznaj vrijednost handle-a "Motor" karakteristike i omogući slanje
+    # obavijesti putem te karakteristike (zapis "setup" koda)
     motor_char = ESP32.getCharacteristics(uuid = MOTOR_UUID)[0]
-    motor_handle = motor_char.getHandle() + 1
-    #print(motor_handle)
-    
-    ESP32.writeCharacteristic(motor_handle, setup_data, True)
-    
-    ball_delegate = PeripheralDelegate(receive_notify)
-    ESP32.setDelegate(ball_delegate)
-    
-    ball_char = ESP32.getCharacteristics(uuid = BALL_UUID)[0]
-    ball_handle = ball_char.getHandle() + 1
-    #print(reket_handle)
-    
-    ESP32.writeCharacteristic(ball_handle, setup_data, True)
-    
-    print ("Waiting for notifications...")
+    motor_handle = motor_char.getHandle()
+    ESP32.writeCharacteristic(motor_handle + 1, setup_data, True)
+
+    # Saznaj vrijednost handle-a "Mechanism" karakteristike i omogući slanje
+    # obavijesti putem te karakteristike (zapis "setup" koda)
+    mechanism_char = ESP32.getCharacteristics(uuid = MECHANISM_UUID)[0]
+    mechanism_handle = mechanism_char.getHandle()
+    ESP32.writeCharacteristic(mechanism_handle + 1, setup_data, True)
+
+    print("Waiting...")
     while True:
-        
         if (ESP32.waitForNotifications(60)):
-            #print("Finished waiting...")
             pass
         else:
             print("Disconnecting from ESP32 device...")
             ESP32.disconnect()
-            
-            left.stop()
-            right.stop()
-            servo1.stop()
-            servo2.stop()
             GPIO.cleanup()
     
 except KeyboardInterrupt:
-    print("Canceling bluetooth ...")
-    #client_sock.close()
     ESP32.disconnect()
-    
-    left.stop()
-    right.stop()
-    servo1.stop()
-    servo2.stop()
     GPIO.cleanup()
+    
